9cf550d9937b489e02f96ca99afe8b5f974ae67a
AkkenapalliBhavya <157678253+AkkenapalliBhavya@users.noreply.github.com>
2024-01-25 14:44:32 +0530

    Add files via upload

---
+++ P9RDGIHJ04_Color Picker/Color Picker.js
@@ -0,0 +1,21 @@
+function button1() {
+    document.getElementById("bg").style.backgroundColor = "#e0e0e0"
+    document.getElementById("span1").textContent = " #e0e0e0"
+
+}
+
+function button2() {
+    document.getElementById("bg").style.backgroundColor = "#6fcf97"
+    document.getElementById("span1").textContent = " #6fcf97"
+}
+
+function button3() {
+    document.getElementById("bg").style.backgroundColor = "#56ccf2 "
+    document.getElementById("span1").textContent = "#56ccf2 "
+}
+
+function button4() {
+    document.getElementById("bg").style.backgroundColor = "#bb6bd9 "
+    document.getElementById("span1").textContent = " #bb6bd9 "
+
+}
