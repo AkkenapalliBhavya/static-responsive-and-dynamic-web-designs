96517def026a7932ab13460d5c80e74e870005d4
AkkenapalliBhavya <157678253+AkkenapalliBhavya@users.noreply.github.com>
2024-01-25 14:44:00 +0530

    Add files via upload

---
+++ 9Q3YUD4KXR_traffic light/traffic light.js
@@ -0,0 +1,14 @@
+function bt1() {
+    document.getElementById("b1").style.backgroundColor = "red"
+    document.getElementById("l1").style.backgroundColor = "red"
+}
+
+function bt2() {
+    document.getElementById("b2").style.backgroundColor = "yellow"
+    document.getElementById("l2").style.backgroundColor = "yellow"
+}
+
+function bt3() {
+    document.getElementById("b3").style.backgroundColor = "green"
+    document.getElementById("l3").style.backgroundColor = "green"
+}
